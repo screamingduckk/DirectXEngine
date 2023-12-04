edbe9b6588734e898f195a730cc9c4e3e9e1cf27
Isabelle Ward <isabelleward@outlook.com>
2023-12-04 22:48:28 +1100

    Add project files.

---
+++ DirectXEngine/AppWindow.cpp
@@ -0,0 +1,26 @@
+#include "AppWindow.h"
+
+
+AppWindow::AppWindow()
+{
+}
+
+
+AppWindow::~AppWindow()
+{
+}
+
+void AppWindow::onCreate()
+{
+	Window::onCreate();
+}
+
+void AppWindow::onUpdate()
+{
+	Window::onUpdate();
+}
+
+void AppWindow::onDestroy()
+{
+	Window::onDestroy();
+}
